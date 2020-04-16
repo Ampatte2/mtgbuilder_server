87835176329133bc9a02ebe4e0fd2607d8d59752
Ampatte2 <Ampatte2@asu.edu>
2020-04-16 15:48:37 -0700

    changed babel resolutions

---
+++ client/build/precache-manifest.d7610806441024b7d69717210b7a9a21.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "958358f6f972dc3b2952761a93128fbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "274d8d7ab0e78cfeb4e1",
+    "url": "/static/css/main.9bf50251.chunk.css"
+  },
+  {
+    "revision": "bf33ecb934202f86f3cc",
+    "url": "/static/js/2.f91ed9e5.chunk.js"
+  },
+  {
+    "revision": "863004d46cd7375f6c043c934b2ed4e3",
+    "url": "/static/js/2.f91ed9e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "274d8d7ab0e78cfeb4e1",
+    "url": "/static/js/main.95a64dfd.chunk.js"
+  },
+  {
+    "revision": "9400ac0538f99217dfc0",
+    "url": "/static/js/runtime-main.76333534.js"
+  },
+  {
+    "revision": "1abe32968473d84f0557ab69035c782d",
+    "url": "/static/media/Banner.1abe3296.jpg"
+  },
+  {
+    "revision": "9f3c168dad9f7beb6108fa6b828234a2",
+    "url": "/static/media/Erasure.9f3c168d.jpg"
+  },
+  {
+    "revision": "2ca7bd5a63de6dfdf12d10e4c17d6262",
+    "url": "/static/media/MagicCardBack.2ca7bd5a.jpg"
+  },
+  {
+    "revision": "bae871f617c02da69329776298696ad6",
+    "url": "/static/media/cardBack.bae871f6.png"
+  }
+]);
