e76b6eecf1a2c09a9835f7c48de90792971bef81
Ampatte2 <Ampatte2@asu.edu>
2020-04-16 16:45:04 -0700

    added new api routing

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-   baseURL: "http://localhost:4000/api"
+   baseURL: "https://mtg-builder-app.herokuapp.com/api"
 })
 
 export const fetchCard = payload => api.post("/getCard", payload).then((response)=>{console.log("Server Response", response); return response}).catch((error)=>console.log(error))
