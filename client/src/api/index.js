deccc4d19b32636819179126cff1cd157146591b
Ampatte2 <Ampatte2@asu.edu>
2020-04-17 10:50:35 -0700

    changed api reference

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-   baseURL: "http://localhost:4000/api"
+   baseURL: "https://mtg-builder-app.herokuapp.com/api"
 })
 
-//https://mtg-builder-app.herokuapp.com/api
+//https://mtg-builder-app.herokuapp.com/api http://localhost:4000/api
 
 export const fetchCard = payload => api.post("/getCard", payload).then((response)=>{console.log("Server Response", response); return response}).catch((error)=>console.log(error))
 
